5a0ba13aa0189e597655233b982e598610c4df13
Eresia <bastien.lepesant@gmail.com>
2016-01-01 22:52:42 +0100

    Ajout des ifdef DEBUG sur les protocoles - serveur

--- server/src/network/Protocole.c
+++ server/src/network/Protocole.c
@@ -17,28 +17,40 @@ void Parse_Protocole (char* datagramme) {
 	// Déplacement
 	if (!strcmp(type, "1")) {
 		char* direction = strtok(NULL, ",");
+
+		#ifdef DEBUG
 		printf("C'est un déplacement vers : %s\n", direction) ;
+		#endif
 	}
 	// Casse bloc
 	else if (!strcmp(type, "2")) {
 		char* position_x_bloc = strtok(NULL, ",") ;
 		char* position_y_bloc = strtok(NULL, ",") ;
 
+		#ifdef DEBUG
 		printf("C'est un cassage du bloc de position x : %s, position y : %s\n", position_x_bloc, position_y_bloc) ;
+		#endif
 	}
 	// Pose bloc
 	else if (!strcmp(type, "3")) {
 		char* position_x_bloc = strtok(NULL, ",") ;
 		char* position_y_bloc = strtok(NULL, ",") ;
 
+		#ifdef DEBUG
 		printf("C'est un posage du bloc de position x : %s, position y : %s\n", position_x_bloc, position_y_bloc) ;
+		#endif
 	}
 	// Message
 	else if (!strcmp(type, "4")) {
 		char* message = strtok(NULL, ",");
+
+		#ifdef DEBUG
 		printf("C'est un message chat : %s\n", message) ;
+		#endif
 	}
 	else {
+		#ifdef DEBUG
 		printf("Erreur, type de requête inconnu\n") ;
+		#endif
 	}
 }
